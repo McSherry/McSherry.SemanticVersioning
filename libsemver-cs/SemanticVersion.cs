763dc8d91e8b39fd3d75ee5f2f368a995a7cc107
Liam McSherry <mcsherry.liam@gmail.com>
2015-09-19 20:52:34 +0100

    Skeleton commit: only the very bare bones of the library

---
+++ libsemver-cs/SemanticVersion.cs
@@ -0,0 +1,200 @@
+﻿// Copyright (c) 2015 Liam McSherry
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace McSherry.SemVer
+{
+    /// <summary>
+    /// <para>
+    /// Represents an immutable Semantic Version. This class cannot be inherited.
+    /// </para>
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// This class represents a Semantic Version compliant with version
+    /// 2.0.0 of the Semantic Versioning specification.
+    /// </para>
+    /// <para>
+    /// Although the specification itself imposes no limit on version
+    /// numbers, this class has the following limitations:
+    /// </para>
+    /// <list type="bullet">
+    ///     <item>
+    ///         <description>
+    ///             The major, minor, and patch versions are represented
+    ///             using an <see cref="int"/>, and so each component's
+    ///             maximum value is 2,147,483,647.
+    ///         </description>
+    ///     </item>
+    /// </list>
+    /// </remarks>
+    public sealed class SemanticVersion
+    {
+        private readonly int _major, _minor, _patch;
+        private readonly List<string> _prIds, _metadata;
+
+        /// <summary>
+        /// <para>
+        /// Creates a new <see cref="SemanticVersion"/> using the
+        /// provided version components.
+        /// </para>
+        /// </summary>
+        /// <param name="major">
+        /// The semantic version's major version.
+        /// </param>
+        /// <param name="minor">
+        /// The semantic version's minor version.
+        /// </param>
+        /// <param name="patch">
+        /// The semantic version's patch version.
+        /// </param>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Thrown when any of <paramref name="major,"/>
+        /// <paramref name="minor"/>, and <paramref name="patch"/>
+        /// is negative.
+        /// </exception>
+        public SemanticVersion(int major, int minor, int patch)
+        {
+            #region Check components aren't negative
+            if (major < 0)
+            {
+                throw new ArgumentOutOfRangeException(
+                    paramName:  nameof(major),
+                    message:    "The major version component cannot be negative."
+                    );
+            }
+
+            if (minor < 0)
+            {
+                throw new ArgumentOutOfRangeException(
+                    paramName:  nameof(minor),
+                    message:    "The minor version component cannot be negative."
+                    );
+            }
+
+            if (patch < 0)
+            {
+                throw new ArgumentOutOfRangeException(
+                    paramName:  nameof(patch),
+                    message:    "The patch version component cannot be negative."
+                    );
+            }
+            #endregion
+
+            _major = major;
+            _minor = minor;
+            _patch = patch;
+
+            _prIds = new List<string>();
+            _metadata = new List<string>();
+
+            // We want the user to be able to work with the pre-release
+            // identifiers / build metadata, but we don't want them to
+            // be able to modify any of them directly.
+            //
+            // We use [AsReadOnly] because it creates a wrapper, so the
+            // user won't be able to cast back to [List<string>] and
+            // make modifications.
+            this.Identifiers = _prIds.AsReadOnly();
+            this.Metadata = _metadata.AsReadOnly();
+        }
+
+        /// <summary>
+        /// <para>
+        /// The semantic version's major version component.
+        /// </para>
+        /// </summary>
+        /// <remarks>
+        /// <para>
+        /// This version component is incremented each time a version with
+        /// a breaking change in it is released. If this is zero, then the
+        /// version number represents an unstable, pre-release version that
+        /// may have breaking changes made at any time without an increment.
+        /// </para>
+        /// </remarks>
+        public int Major => _major;
+        /// <summary>
+        /// <para>
+        /// The semantic version's minor version component.
+        /// </para>
+        /// </summary>
+        /// <remarks>
+        /// <para>
+        /// This version component is incremented each time a version with at
+        /// least one new feature in it is released. This component is reset
+        /// to zero with every <see cref="Major"/> version increment.
+        /// </para>
+        /// </remarks>
+        public int Minor => _minor;
+        /// <summary>
+        /// <para>
+        /// The semantic version's patch version component.
+        /// </para>
+        /// </summary>
+        /// <remarks>
+        /// <para>
+        /// This version component is incremented each time a version with a
+        /// backwards-compatible bug fix is released. This component is reset
+        /// to zero with every <see cref="Major"/> or <see cref="Minor"/>
+        /// version increment.
+        /// </para>
+        /// </remarks>
+        public int Patch => _patch;
+
+        /// <summary>
+        /// <para>
+        /// The pre-release identifier components of the semantic
+        /// version.
+        /// </para>
+        /// </summary>
+        /// <remarks>
+        /// <para>
+        /// Pre-release identifiers are used to indicate a release that
+        /// is a pre-release. For example, <c>1.1.0-rc.1</c> for a release
+        /// candidate.
+        /// </para>
+        /// </remarks>
+        public IReadOnlyList<string> Identifiers
+        {
+            get;
+            private set;
+        }
+        /// <summary>
+        /// <para>
+        /// The build metadata components of the semantic version.
+        /// </para>
+        /// </summary>
+        /// <remarks>
+        /// <para>
+        /// Build metadata components provide additional information about
+        /// a release, such as the time and date it was built and the commit
+        /// identifier of the commit the release was built from.
+        /// </para>
+        /// </remarks>
+        public IReadOnlyList<string> Metadata
+        {
+            get;
+            private set;
+        }
+    }
+}
