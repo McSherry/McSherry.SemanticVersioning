McSherry/McSherry.SemanticVersioning

commit 34ae77e4b9d43127b19db77062c10f652de8a942

Add [SemanticVersion.CompatibleWith] method, with unit tests.
[CompatibleWith] determines whether its parameter is backwards-compatible with the
instance it is called on. There are going to be a few situations where this method
is not suitable (such as comparing 0.x.x versions, as the method always provides
[false] result for these as they are not guaranteed to be compatible).

This commit also changes the [>=] and [<=] operators so that the versions compared
are compared with an equivalency comparison instead of comparing equality. This is
because the operators work with precedence, and exact equality is not prerequisite
for having the same precedence.

libsemver-cs/SemanticVersion.cs
@@ -201,7 +201,7 @@ namespace McSherry.SemanticVersioning
         /// </returns>
         public static bool operator >=(SemanticVersion l, SemanticVersion r)
         {
-            return (l == r) || (l > r);
+            return l.EquivalentTo(r) || (l > r);
         }
         /// <summary>
         /// <para>
@@ -222,7 +222,7 @@ namespace McSherry.SemanticVersioning
         /// </returns>
         public static bool operator <=(SemanticVersion l, SemanticVersion r)
         {
-            return (l == r) || (l < r);
+            return l.EquivalentTo(r) || (l < r);
         }
 
 
@@ -526,6 +526,133 @@ namespace McSherry.SemanticVersioning
                    this.Minor == semver.Minor                           &&
                    this.Patch == semver.Patch                           &&
                    this.Identifiers.SequenceEqual(semver.Identifiers);
+        }
+        /// <summary>
+        /// <para>
+        /// Determines whether the specified <see cref="SemanticVersion"/> is
+        /// backwards-compatible with the current version. 
+        /// </para>
+        /// </summary>
+        /// <param name="semver">
+        /// The <see cref="SemanticVersion"/> to test for backwards
+        /// compatibility.
+        /// </param>
+        /// <returns>
+        /// True if <paramref name="semver"/> is backwards-compatible with
+        /// the current version. False if otherwise.
+        /// </returns>
+        /// <remarks>
+        /// <para>
+        /// The following situations will always produce a false result:
+        /// </para>
+        /// <list type="bullet">
+        ///     <item>
+        ///         <description>
+        ///             The <see cref="Major"/> versions of the compared
+        ///             <see cref="SemanticVersion"/>s differ.
+        ///         </description>
+        ///     </item>
+        ///     <item>
+        ///         <description>
+        ///             The <see cref="Major"/> versions of either of the
+        ///             compared versions are equal to zero (unless the
+        ///             two versions are equivalent).
+        ///         </description>
+        ///     </item>
+        ///     <item>
+        ///         <description>
+        ///             The parameter <paramref name="semver"/> is null.
+        ///         </description>
+        ///     </item>
+        /// </list>
+        /// <para>
+        /// If none of the above conditions are met, compatibility is determined
+        /// through simple precedence comparison, where a version will only ever
+        /// be considered compatible if it is of equal or greater precedence.
+        /// </para>
+        /// <para>
+        /// It should be noted that a <paramref name="semver"/> value with
+        /// pre-release identifiers will be considered backwards-compatible 
+        /// provided its
+        /// <see cref="Major"/>-<see cref="Minor"/>-<see cref="Patch"/>
+        /// trio is greater than the trio of this version and the
+        /// <see cref="Major"/> versions are equal. This is because, even
+        /// though it is a pre-release version, it is within the same
+        /// major version, and so should, if the Semantic Versioning
+        /// specification is being properly adhered to, be backwards-compatible.
+        /// </para>
+        /// </remarks>
+        public bool CompatibleWith(SemanticVersion semver)
+        {
+            // If it's null, it definitely can't be compatible.
+            if (object.ReferenceEquals(semver, null))
+                return false;
+
+            // If the version is equivalent to us, then we're obviously 
+            // compatible because we're the same version.
+            if (this.EquivalentTo(semver))
+                return true;
+
+            // A change in the major version indicates a breaking change, so
+            // if they don't have the same major version we cannot be sure
+            // that they will be compatible.
+            if (this.Major != semver.Major)
+                return false;
+
+            // A major version of zero indicates that the version is not yet
+            // stable, and each new release may contain breaking changes. As a
+            // result, we can't determine if the versions are compatible.
+            if (this.Major == 0 || semver.Major == 0)
+                return false;
+
+            // This is to determine whether a version with pre-release identifers
+            // is backwards-compatible with this version.
+            //
+            // We first check to see whether we have any identifiers. We can only
+            // proceed if we don't.
+            //
+            // If we don't have any identifiers, we check to see whether the other
+            // version does. If it doesn't have any, we don't need to perform this
+            // check.
+            //
+            // If the other version has identifiers, it can only be considered
+            // backwards-compatible if either of the following conditions are
+            // true:
+            //
+            //      - The other version's minor version is greater; or
+            //      - The minor versions are equal, but the other version
+            //        has a greater patch version than we do.
+            //
+            // If either of the conditions are met, it is backwards-compatible and
+            // we can return [true].
+            //
+            // We don't need to check to make sure the major versions are the same
+            // because that check has already been made earlier in the method.
+            if (!this.Identifiers.Any() && semver.Identifiers.Count > 0 &&
+                ((semver.Minor > this.Minor) ||
+                 (semver.Minor == this.Minor && semver.Patch > this.Patch)))
+                return true;
+
+            // If this is a pre-release version, the other version isn't, and
+            // the above test failed, then the other version is not backwards-
+            // -compatible with this version.
+            if (this.Identifiers.Any() && !semver.Identifiers.Any())
+                return false;
+
+            // If both versions are pre-release versions, they cannot be
+            // compatible because neither has a finalised/stable API that it
+            // depends on.
+            if (this.Identifiers.Any() && semver.Identifiers.Any())
+                return false;
+
+            // If we've passed all the checks, we know that the versions have
+            // the same major version but are not equivalent. To determine if
+            // [semver] is backwards-compatible, we need to check to make sure
+            // it has greater precedence than we do.
+            //
+            // [semver] must have greater precedence because a version with
+            // lesser precedence may not contain features that we rely on.
+            return semver > this;
         }
 
         // object Overrides

libsemver-cs.Testing/SemanticVersionTests.cs
@@ -498,5 +498,152 @@ namespace McSherry.SemanticVersioning
             Assert.IsTrue(!sv2.EquivalentTo(sv3) && !sv3.EquivalentTo(sv2),
                           "Inequivalence not commutative (2).");
         }
+        /// <summary>
+        /// <para>
+        /// Tests to make sure that the behaviour of the
+        ///  <see cref="SemanticVersion.CompatibleWith(SemanticVersion)"/>
+        /// method is as expected.
+        /// </para>
+        /// </summary>
+        [TestMethod, TestCategory(Category)]
+        public void Compatibility()
+        {
+            // Backwards-compatibility determination is not commutative,
+            // so a few of these tests will just be reversed parameters.
+
+            #region "Always False" checks (numbers 0 to 10)
+            {
+                // First thing we're going to do is a basic test of the "always
+                // false" conditions listed in the remarks for [CompatibleWith].
+                // These are:
+                //
+                //      - Major version of zero on either version
+                //      - Different Major versions
+                //      - Null [SemanticVersion] instance
+                //
+                // We can't really test here that these are *always* true, but
+                // that doesn't mean we shouldn't try testing them anyway.
+                var af_sv0 = new SemanticVersion(0, 5, 0);
+                var af_sv1 = new SemanticVersion(0, 6, 0);
+                var af_sv2 = new SemanticVersion(1, 2, 0);
+                var af_sv3 = new SemanticVersion(2, 2, 0);
+                var af_sv4 = (SemanticVersion)null;
+
+                // Zero major versions
+                Assert.IsFalse(af_sv0.CompatibleWith(af_sv1),
+                               "Incorrect compatibility (0).");
+                Assert.IsFalse(af_sv1.CompatibleWith(af_sv0),
+                               "Incorrect compatibility (1).");
+                Assert.IsFalse(af_sv1.CompatibleWith(af_sv2),
+                               "Incorrect compatibility (2).");
+                Assert.IsFalse(af_sv2.CompatibleWith(af_sv1),
+                               "Incorrect compatibility (3).");
+                // Zero major versions, equivalent versions
+                Assert.IsTrue(af_sv1.CompatibleWith(af_sv1),
+                               "Incorrect compatibility (4).");
+
+                // Different major versions
+                Assert.IsFalse(af_sv2.CompatibleWith(af_sv3),
+                               "Incorrect compatibility (5).");
+                Assert.IsFalse(af_sv3.CompatibleWith(af_sv2),
+                               "Incorrect compatibility (6).");
+
+                // Null version
+                Assert.IsFalse(af_sv0.CompatibleWith(af_sv4),
+                               "Incorrect compatibility (7).");
+                Assert.IsFalse(af_sv1.CompatibleWith(af_sv4),
+                               "Incorrect compatibility (8).");
+                Assert.IsFalse(af_sv2.CompatibleWith(af_sv4),
+                               "Incorrect compatibility (9).");
+                Assert.IsFalse(af_sv3.CompatibleWith(af_sv4),
+                               "Incorrect compatibility (10).");
+            }
+            #endregion
+            #region Pre-release Versions (numbers 11 to 22)
+            {
+                // Pre-release versions are a bit more difficult with
+                // their compatibility. See [CompatibleWith] remarks
+                // and code comments for more information.
+
+                // No pre-release identifiers for the "control" comparison.
+                //
+                // This should be compatible with [pr_sv3] and [pr_sv4] as
+                // they are both pre-release versions of versions later than
+                // this one, so they should (for standards compliance) implement
+                // the APIs this depends on.
+                var pr_sv0 = new SemanticVersion(1, 0, 0);
+
+                // These two can't be compatible because they are equal in all
+                // but pre-release identifiers.
+                var pr_sv1 = new SemanticVersion(1, 0, 0, new[] { "rc", "1" });
+                var pr_sv2 = new SemanticVersion(1, 0, 0, new[] { "rc", "2" });
+
+                Assert.IsFalse(pr_sv0.CompatibleWith(pr_sv1),
+                               "Incorrect compatibility (11).");
+                Assert.IsFalse(pr_sv1.CompatibleWith(pr_sv0),
+                               "Incorrect compatibility (12).");
+
+                Assert.IsFalse(pr_sv0.CompatibleWith(pr_sv2),
+                               "Incorrect compatibility (13).");
+                Assert.IsFalse(pr_sv2.CompatibleWith(pr_sv0),
+                               "Incorrect compatibility (14).");
+
+                Assert.IsFalse(pr_sv1.CompatibleWith(pr_sv2),
+                               "Incorrect compatibility (15).");
+                Assert.IsFalse(pr_sv2.CompatibleWith(pr_sv1),
+                               "Incorrect compatibility (16).");
+
+                // These two can't be compatible because they are both pre-release
+                // versions.
+                var pr_sv3 = new SemanticVersion(1, 1, 0, new[] { "rc" });
+                var pr_sv4 = new SemanticVersion(1, 2, 0, new[] { "rc" });
+
+                Assert.IsFalse(pr_sv3.CompatibleWith(pr_sv4),
+                               "Incorrect compatibility (17).");
+                Assert.IsFalse(pr_sv4.CompatibleWith(pr_sv3),
+                               "Incorrect compatibility (18).");
+
+                Assert.IsTrue(pr_sv0.CompatibleWith(pr_sv3),
+                               "Incorrect compatibility (19).");
+                Assert.IsFalse(pr_sv3.CompatibleWith(pr_sv0),
+                               "Incorrect compatibility (20).");
+
+                Assert.IsTrue(pr_sv0.CompatibleWith(pr_sv4),
+                               "Incorrect compatibility (21).");
+                Assert.IsFalse(pr_sv4.CompatibleWith(pr_sv0),
+                               "Incorrect compatibility (22).");
+
+            }
+            #endregion
+            #region Regular checks (numbers 23 to 30)
+            {
+                var sv0 = new SemanticVersion(1, 0, 0);
+                var sv1 = new SemanticVersion(1, 1, 0);
+
+                Assert.IsTrue(sv0.CompatibleWith(sv1),
+                              "Incorrect compatibility (23).");
+                Assert.IsFalse(sv1.CompatibleWith(sv0),
+                               "Incorrect compatibility (24).");
+
+                var sv2 = new SemanticVersion(2, 0, 0);
+                var sv3 = new SemanticVersion(2, 2, 0);
+
+                Assert.IsTrue(sv2.CompatibleWith(sv3),
+                              "Incorrect compatibility (25).");
+                Assert.IsFalse(sv3.CompatibleWith(sv2),
+                               "Incorrect compatibility (26).");
+
+                Assert.IsFalse(sv0.CompatibleWith(sv2),
+                               "Incorrect compatibility (27).");
+                Assert.IsFalse(sv1.CompatibleWith(sv2),
+                               "Incorrect compatibility (28).");
+
+                Assert.IsFalse(sv0.CompatibleWith(sv3),
+                               "Incorrect compatibility (29).");
+                Assert.IsFalse(sv1.CompatibleWith(sv3),
+                               "Incorrect compatibility (30).");
+            }
+            #endregion
+        }
     }
 }

commit f8f2cb1ab4173f9ef3e3076d73c2a4c855484d54

Amends [>=] and [<=] behaviour change in 34ae77e.
The change in 34ae77e broke the behaviour (NRE on null argument) and I didn't notice
that it did so. This change incorporates the best of both: we first (safely) use the
[EquivalentTo] method. If and only if that doesn't work, we test equality.

libsemver-cs/SemanticVersion.cs
@@ -201,7 +201,7 @@ namespace McSherry.SemanticVersioning
         /// </returns>
         public static bool operator >=(SemanticVersion l, SemanticVersion r)
         {
-            return l.EquivalentTo(r) || (l > r);
+            return (l?.EquivalentTo(r) == true || l == r) || (l > r);
         }
         /// <summary>
         /// <para>
@@ -222,7 +222,7 @@ namespace McSherry.SemanticVersioning
         /// </returns>
         public static bool operator <=(SemanticVersion l, SemanticVersion r)
         {
-            return l.EquivalentTo(r) || (l < r);
+            return (l?.EquivalentTo(r) == true || l == r) || (l < r);
         }
 
 