8146fa0ac17ae0385b44c41c576bb146b9d98a20
Liam McSherry <mcsherry.liam@gmail.com>
2019-06-14 18:46:17 +0100

    Add .NET Standard test target, update dependency.
    It looks like .NET Standard doesn't include [GuidAttribute]. As it's
    not relevant in our use case, this commit removes it.

--- McSherry.SemanticVersioning.Testing/Properties/AssemblyInfo.cs
+++ McSherry.SemanticVersioning.Testing/Properties/AssemblyInfo.cs
@@ -38,9 +38,6 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1094d088-aac1-457a-b515-652b09bc11bd")]
-
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
